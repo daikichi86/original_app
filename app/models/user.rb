21ba25f76ef81963b8ca3655c08d7f89d0cd4627
daikichi86 <maskman-daikigx@i.softbank.jp>
2021-01-20 23:12:34 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+  end
+       
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: 'Include both letters and numbers' }
+       
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    nickname { 'タロー' }
+    email { 'sample@sample.com' }
+    password { 'test1111' }
+    password_confirmation { password }
+  end
+end
+
